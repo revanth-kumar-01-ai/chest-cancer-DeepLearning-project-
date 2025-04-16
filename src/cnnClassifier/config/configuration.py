93e2a0eceb8aabf1d27b1c093a7541a962a83e37
revanth-kumar-01-ai <revanthj486@gmail.com>
2025-04-16 14:26:31 +0530

    model eval

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,11 @@
 from cnnClassifier.constants import * 
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from cnnClassifier.entity.config_entity import (
+                                                    DataIngestionConfig, 
+                                                    PrepareBaseModelConfig, 
+                                                    TrainingConfig, 
+                                                    EvaluationConfig
+                                                )
 import os 
 
 class ConfigurationManager:
@@ -72,3 +77,17 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    # Evaluation 
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/revanth-kumar-01-ai/chest-cancer-DeepLearning-project-.mlflow", # MlFlow URL
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+    
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,7 +2,7 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion 
 from cnnClassifier import logger 
 
-STAGE_NAME = "Data ingestion stage"
+STAGE_NAME = 'Data ingestion stage'
 
 class DataIngestionTrainingPipeline:
     def __init__(self):
